56eb1d11ca783fc7de3bd9b9b170550977ce65b4
Jonathan March <jmarch@wikia-inc.com>
2012-11-14 00:24:11 +0000

    Removing script for unused table.

--- sql/qdup_job_logs.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE qdup_job_logs (
-    id            INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    stdout        TEXT,
-    stderr        TEXT,
-    updated_at    TIMESTAMP,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB;
-
