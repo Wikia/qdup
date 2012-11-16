19549fab91a65dd7b25b8d053e0027342fb0e081
Jonathan March <jmarch@wikia-inc.com>
2012-11-16 00:01:38 +0000

    Added TIMEDOUT status to qdup_jobs and changed worker column to be a fixed-length CHAR.

--- sql/qdup_jobs.sql
+++ sql/qdup_jobs.sql
@@ -2,14 +2,15 @@ CREATE TABLE qdup_jobs (
     id            INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     queue         VARCHAR(255) NOT NULL DEFAULT 'main',
     class         VARCHAR(255),
-    status        ENUM('ON HOLD','WAITING','RUNNING','COMPLETED','FAILED') DEFAULT 'WAITING',
+    status        ENUM('ON HOLD','WAITING','RUNNING','TIMEDOUT','FAILED','COMPLETED') DEFAULT 'WAITING',
     priority      TINYINT UNSIGNED DEFAULT 100,
     updated_at    TIMESTAMP,
     run_after     DATETIME DEFAULT '1970-01-01',
     begin_time    DATETIME,
     end_time      DATETIME,
-    worker        VARCHAR(255),
+    worker        CHAR(16) NOT NULL DEFAULT '',
     args          VARCHAR(2048) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
 --  <custom columns>
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+
