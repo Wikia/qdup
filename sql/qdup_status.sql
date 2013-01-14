5a3fa70c0132427fcac6572d6b93cb36867f3c52
Jonathan March <jmarch@wikia-inc.com>
2013-01-14 18:51:59 +0000

    Minor tweaks to status view.

--- sql/qdup_status.sql
+++ sql/qdup_status.sql
@@ -2,6 +2,7 @@ DROP VIEW IF EXISTS qdup.qdup_status;
 
 CREATE VIEW qdup.qdup_status AS
 SELECT class,
+       queue,
        priority,
        COUNT(CASE WHEN status = 'ON HOLD'   THEN 1 ELSE null END) AS on_hold,
        COUNT(CASE WHEN status = 'WAITING'   THEN 1 ELSE null END) AS waiting,
@@ -11,9 +12,6 @@ SELECT class,
        COUNT(CASE WHEN status = 'COMPLETED' THEN 1 ELSE null END) AS completed,
        COUNT(1) AS total,
        ROUND(100 * COUNT(CASE WHEN status = 'COMPLETED' THEN 1 ELSE null END) / COUNT(1), 2) AS percent,
-       COUNT(CASE WHEN status = 'COMPLETED'
-                   AND begin_time > now() - INTERVAL 10 MINUTE
-                   AND end_time   > now() - INTERVAL 10 MINUTE THEN 1 ELSE null END) AS last_10_min,
        ROUND(60 * COUNT(CASE WHEN status = 'COMPLETED'
                               AND begin_time > now() - INTERVAL 10 MINUTE
                               AND end_time   > now() - INTERVAL 10 MINUTE THEN 1 ELSE null END) /
@@ -22,5 +20,6 @@ SELECT class,
                                                                     AND end_time   > now() - INTERVAL 10 MINUTE THEN begin_time ELSE null END))), 2) AS jobs_per_min
   FROM qdup.qdup_jobs
  GROUP BY class,
+          queue,
           priority
 
