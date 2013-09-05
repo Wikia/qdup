ce62ddb1a7dac75303c41555990f02e2210a06b3
Jonathan March <jmarch@wikia-inc.com>
2013-09-05 00:06:41 +0000

    Updated NOT NULL column and changed worker to be beyond 16 characters.

--- sql/qdup_jobs.sql
+++ sql/qdup_jobs.sql
@@ -1,16 +1,15 @@
-CREATE TABLE qdup_jobs (
+CREATE TABLE qdup_jobs_new (
     id            INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
     queue         VARCHAR(255) NOT NULL DEFAULT 'main',
-    class         VARCHAR(255),
+    class         VARCHAR(255) NOT NULL,
     status        ENUM('ON HOLD','WAITING','RUNNING','TIMEDOUT','FAILED','COMPLETED') DEFAULT 'WAITING',
     priority      TINYINT UNSIGNED DEFAULT 100,
     updated_at    TIMESTAMP,
     run_after     DATETIME DEFAULT '1970-01-01',
     begin_time    DATETIME,
     end_time      DATETIME,
-    worker        CHAR(16) NOT NULL DEFAULT '',
+    worker        VARCHAR(255) DEFAULT '',
     args          VARCHAR(2048) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '',
 --  <custom columns>
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
-
